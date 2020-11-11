89c2f037fac47cd2c55914bcf1cd4b5cc7178e6a
Gerardo Manuel Muñoz <gerardo.munoz@toctoc.com>
2020-11-11 00:53:21 -0300

    backend hecho

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const awsServerlessExpress = require('aws-serverless-express')
 var express = require('express')
+var axios = require('axios')
 
 var app = express()
 app.use('/', express.static('./frontend/build/'))
@@ -8,8 +9,100 @@ const server = awsServerlessExpress.createServer(app)
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
 
-exports.poliza = (event, context) => {
-    return {
-        statusCode: 200, body: JSON.stringify({ message: "", error: "" })
+exports.poliza = async (event, context) => {
+    console.log("hola1")
+    let retorno = {
+        statusCode: 500, body: { message: "", error: "" }
     }
+    const hijos = [
+        {
+            costo: 0.279,
+            dental: 0.12
+        },
+        {
+            costo: 0.4396,
+            dental: 0.1950
+        },
+        {
+            costo: 0.5599,
+            dental: 0.2480
+        }
+    ]
+    //mayor a 65 años no tiene cobertura
+    //El % de la empresa es el costo que asumirá la empresa del costo total de la póliza, el resto es cubierto por cada empleado.
+    return new Promise(async (resolve,reject) => {
+        console.log("hola2")
+        return axios.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/policy')
+        .then(async policy => {
+            console.log("hola3")
+            if(policy.status !== 200){
+                retorno.body.error = "API no responde"
+                return reject(retorno)
+            }
+            if(policy.data.policy === undefined && !policy.data.policy.success){
+                retorno.body.error = "Error en la respuesta de la API"
+                return reject(retorno)
+            }
+
+            let _policy = policy.data.policy
+
+            if(!_policy.workers.length){
+                retorno.statusCode = 404
+                retorno.body.error = "No existen trabajadores"
+                return reject(retorno)
+            }
+
+            console.log("hola4")
+            
+            return Promise.all(_policy.workers.map(async x => {
+                console.log("hola6")
+                const costoEmpresa = async (has_dental_care, { age, childs }) => {
+                    console.log("hola7", has_dental_care, age, childs )
+                    let retorno = 0
+
+                    if(age > 65) return retorno
+
+                    if(childs === 0) retorno += hijos[0].costo
+                    if(childs === 1) retorno += hijos[1].costo
+                    if(childs >= 2) retorno += hijos[2].costo
+
+                    if(has_dental_care){
+                        if(childs === 0) retorno += hijos[0].dental
+                        if(childs === 1) retorno += hijos[1].dental
+                        if(childs >= 2) retorno += hijos[2].dental
+                    }
+
+                    return retorno
+                    
+                }
+                var costo = await costoEmpresa(_policy.has_dental_care, x)
+                var costoCobertura = +(Math.abs( costo * (_policy.company_percentage / 100))).toFixed(2)
+                var costoEmpleado = +(costo - costoCobertura).toFixed(2)
+                return {
+                    ...x,
+                    costoCobertura: costoCobertura,
+                    copagoEmpleado: costoEmpleado,
+                }
+            }))
+            .then(result => {
+                console.log("hola8", result )
+                retorno.statusCode = 200
+                retorno.body.message = result
+                return resolve(retorno)
+            })
+
+        })
+    })
+    .then(async r => {
+        r.body = JSON.stringify(r.body)
+        return r
+    })
+    .catch(async error => {
+        retorno.body.error = error
+        retorno.body.message = "Error"
+
+        retorno.body = JSON.stringify(retorno.body)
+        // Sentry.captureException(error)
+        return retorno
+    })
 }
